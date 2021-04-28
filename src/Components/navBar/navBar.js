d89d1d9081a01a695e3b0ac55bdecf34d2f7980a
muhammadlovlu <chyshahab@gmail.com>
2021-04-28 08:57:38 +0600

    updated

--- src/Components/navBar/navBar.js
+++ src/Components/navBar/navBar.js
@@ -10,14 +10,14 @@ const navBar = () => {
         <Navbar.Brand><img src={logo} alt="logo" className="img-height" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
         <Navbar.Collapse id="basic-navbar-nav " className="text-primary" >
-          <Nav className="mr-auto bg-dark">
+          <Nav className="mr-auto bg-dark text-light">
             <Nav.Link href="#home" className="text-white newColor">Home</Nav.Link>
-            <Nav.Link href="#link" className="text-white newColor">Link</Nav.Link>
-            <NavDropdown title="Cars" id="basic-nav-dropdown newColor">
-              <NavDropdown.Item href="#action/3.1" className="text-light newColor">GMC</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2" className="text-light newColor">Cadillac</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" className="text-light newColor">Lamborghini</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4" className="text-light newColor">Rolls Royce</NavDropdown.Item>
+            <Nav.Link href="#link" className="text-white newColor">About</Nav.Link>
+            <NavDropdown title="Cars" id="basic-nav-dropdown " className="text-light bg-dark">
+              <NavDropdown.Item href="#action/3.1" className="text-light bg-dark">GMC</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" className="text-light bg-dark">Cadillac</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className="text-light bg-dark">Lamborghini</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4" className="text-light bg-dark">Rolls Royce</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
